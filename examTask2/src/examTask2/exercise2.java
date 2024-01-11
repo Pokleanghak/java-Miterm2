e57806601f4eb522817d635f25b136ea5a1a1bb1
Pokleanghak <156267168+Pokleanghak@users.noreply.github.com>
2024-01-11 08:58:56 +0700

    Add files via upload

---
+++ examTask2/src/examTask2/exercise2.java
@@ -0,0 +1,37 @@
+package examTask2;
+
+	class Rectangle {
+	    private double length;
+	    private double width;
+
+	    public Rectangle(double length, double width) {
+	        this.length = length;
+	        this.width = width;
+	    }
+
+	    public double getArea() {
+	        return length * width;
+	    }
+
+	    public double getPerimeter() {
+	        return 2 * (length + width);
+	    }
+	}
+
+	class Square extends Rectangle {
+	    public Square(double sideLength) {
+	        super(sideLength, sideLength);
+	    }
+	}
+	
+	public class Main {
+	    public static void main(String[] args) {
+	        Square square = new Square(5.0);
+	        double area = square.getArea();
+	        double perimeter = square.getPerimeter();
+
+	        System.out.println("Square Area: " + area);
+	        System.out.println("Square Perimeter: " + perimeter);
+	    }
+	}
+
